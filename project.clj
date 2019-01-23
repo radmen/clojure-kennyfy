ef9da3960f88669c4c7ac78981a9bc1972fc8fb5
Radoslaw Mejer <radmen@radmen.info>
2019-01-23 09:13:25 +0100

    fixup! Simplify namespace of package

--- project.clj
+++ project.clj
@@ -4,5 +4,5 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.9.0"]]
-  :repl-options {:init-ns info.radmen.kennyfy}
+  :repl-options {:init-ns radmen.kennyfy}
   :plugins [[lein-cljfmt "0.6.4"]])
